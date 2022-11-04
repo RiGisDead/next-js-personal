392d241a513393454bb7260c22f7153d5d540a21
Akthem <RiGisDead@users.noreply.github.com>
2022-11-04 01:22:59 -0500

    Edit

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Wooo</title>
+        <title>I LOVE YOU KIA!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
